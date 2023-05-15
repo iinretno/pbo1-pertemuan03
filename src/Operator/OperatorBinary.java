f255fd1745336572e368b399e95bc09d4f0fef0b
iinretno <133147886+iinretno@users.noreply.github.com>
2023-05-16 01:53:31 +0800

    Operator Unary

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,23 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan di program
+
+    String nama = "Retno Indriany";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.68;
+
+    public static void main(String[] args){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+
+
+}
